03a32f6df01033e0d74b707dbc538d190316dd20
yazdipour <shahriar.yazdipour@outlook.com>
2020-03-11 01:01:33 +0100

    Cleaning and separating audio ctrl class

---
+++ MuteSpotifyAds/AudioControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MuteSpotifyAds
+{
+    internal static class AudioControl
+    {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int WM_APPCOMMAND = 0x319;
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+        public static void ToggleMute(IntPtr handle) { SendMessageW(handle, WM_APPCOMMAND, handle, (IntPtr)APPCOMMAND_VOLUME_MUTE); }
+    }
+}
--- MuteSpotifyAds/Program.cs
+++ MuteSpotifyAds/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
+using System.Linq;
 
 namespace MuteSpotifyAds
 {
@@ -8,23 +7,11 @@ namespace MuteSpotifyAds
     {
         private static void Main(string[] args)
         {
-            ProcessLookUp(Process.GetCurrentProcess().MainWindowHandle);
+            if(ProcessLookUp("Spotify", "Advertisement"))
+                AudioControl.ToggleMute(Process.GetCurrentProcess().MainWindowHandle);
         }
 
-        private static void ProcessLookUp(IntPtr windowHandle)
-        {
-            foreach (var p in Process.GetProcessesByName("Spotify"))
-                if (p.MainWindowTitle == "Advertisement")
-                    AudioControl.ToggleMute(windowHandle);
-        }
-    }
-
-    internal static class AudioControl
-    {
-        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
-        private const int WM_APPCOMMAND = 0x319;
-        [DllImport("user32.dll")]
-        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
-        public static void ToggleMute(IntPtr handle) { SendMessageW(handle, WM_APPCOMMAND, handle, (IntPtr)APPCOMMAND_VOLUME_MUTE); }
+        private static bool ProcessLookUp(string processName, string triggerOn) 
+            => Process.GetProcessesByName(processName).Any(p => p.MainWindowTitle == triggerOn);
     }
 }
