b8f7bc774bd5f7d3758cf25fb28a4fc17d24c0e6
yazdipour <shahriar.yazdipour@outlook.com>
2020-03-11 01:36:14 +0100

    Main feature working

--- MuteSpotifyAds/Program.cs
+++ MuteSpotifyAds/Program.cs
@@ -1,14 +1,28 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 
 namespace MuteSpotifyAds
 {
     internal class Program
     {
+        private static bool _isMuted, _isAd;
         private static void Main(string[] args)
         {
-            if(ProcessLookUp("Spotify", "Advertisement"))
+            var t = new Timer(TimerCallback, null, 0, 1000);
+            Console.WriteLine("Waiting for Ads (=_=)\n\nEnter to Stop...");
+            Console.ReadLine();
+        }
+
+        private static void TimerCallback(object _)
+        {
+            _isAd = ProcessLookUp("Spotify", "Advertisement");
+            if ((!_isMuted && _isAd) || (_isMuted && !_isAd))
+            {
+                _isMuted = !_isMuted;
                 AudioControl.ToggleMute(Process.GetCurrentProcess().MainWindowHandle);
+            }
         }
 
         private static bool ProcessLookUp(string processName, string triggerOn) 
